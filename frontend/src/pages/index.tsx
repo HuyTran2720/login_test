e2b7881be795c25915857a60bf74d90e40be06f9
HuyTran2720 <ht2005790@gmail.com>
2025-08-02 22:03:14 +1000

    - signup skelelton - not connected to db - doesnt do anything - no proper validation

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,8 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
       <main >
-
+        <Link href={"signup"}>
+          <Button>
+            Sign Up
+          </Button>
+        </Link>
+        <Link href={"login"}>
+          <Button>
+            Login
+          </Button>
+        </Link>
       </main>
       <footer>
 
